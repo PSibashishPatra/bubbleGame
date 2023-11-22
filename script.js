32f389bdff5b5111ca3a9afdc1b0b88e968014e7
PSibashishPatra <psibashishpatra@gmail.com>
2023-11-22 21:17:33 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-var clutter = "";
+var timer = 60 ;
+var score = 0 ;
 
-for(var i=1; i<=70; i++) {
-    clutter += `<div class="bubble">3</div>` ;
+
+function increaseScore(){
+    score += 10 ;
+    document.querySelector("#scoreval").textContent = score ;
+
+}
+
+
+function getNewHit(){
+    var rn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = rn ;
+}
+
+function makeBubble() {
+    var clutter = "";
+
+for(var i=1; i<=126; i++) {
+    var rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>` ;
 }
 
 document.querySelector("#pbtm").innerHTML = clutter ;
+
+}
+
+
+function runTimer(){
+    var timerint = setInterval(function(){
+        if(timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer ;
+        }
+        else{
+            clearInterval(timerint);
+        }
+    },1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click",function (details) {
+    console.log(Number(details.target.textContent));
+});
+
+getNewHit();
+runTimer();
+makeBubble();
